8a2072f609484e5d80774ff88c04602e0f952bd3
paweltar <paweltar@gmail.com>
2017-08-02 21:43:38 +0200

    Fetch all necessary data from github api concurrently with axios

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const bodyParser = require('body-parser');
 const battle = require('./battle.js');
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'landing-page/dist')));
 
 const apikey = process.env.SLACK_API_TOKEN;
@@ -17,34 +17,49 @@ const apikey = process.env.SLACK_API_TOKEN;
 app.set('port', (process.env.PORT || 9001));
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/landing-page/dist/index.html');
+  res.sendFile(__dirname + '/landing-page/dist/index.html');
 });
 
 app.get('/hello', (req, res) => {
-       res.send('hello');
+  res.send('hello');
 });
 
 app.post('/', (req, res) => {
-    const opponent = req.body.text.split(" ");
-    const currentUser = req.body.user_name;
-    const body = {
-        response_type: "in_channel",
-        "attachments": [{
-            "text": `Let's start battle @${opponent[0]} VS @${opponent[1]}! Who will be the winner?`
-        }]
-    };
-    battle.getData(opponent[0]).then(text=>{
-        console.log(text);
-    body.text = text;
-    res.send(body);
-}).catch(err => {
-  console.error('An error occurred making this request');
-  console.error(err.message);
-});
-    
-});
+  const opponents = req.body.text.split(" ");
+  const firstUser = opponents[0];
+  const secondUser = opponents[1];
 
+  const body = {
+    response_type: "in_channel",
+    "attachments": [
+      {
+        "text": `Let's start battle @${firstUser} VS @${secondUser}! Who will be the winner?`
+      }
+    ]
+  };
+
+  battle.getData([firstUser, secondUser]).then(text => {
+    const output = `user: ${text[0].username}
+                    /stars: ${text[0].stars}
+                    /fork:${text[0].forks}
+                    /watch:${text[0].watches}
+                    /followers:${text[0].followers}
+                    --------------------------------
+                    user: ${text[1].username}
+                    /stars: ${text[1].stars}
+                    /fork:${text[1].forks}
+                    /watch:${text[1].watches}
+                    /followers:${text[1].followers}`;
+
+    console.log(output);
+    body.text = output;
+    res.send(body);
+  }).catch(err => {
+    console.error('An error occurred making this request');
+    console.error(err.message);
+  });
+});
 
 app.listen(app.get('port'), function() {
-    console.log('Node app is running on port', app.get('port'));
+  console.log('Node app is running on port', app.get('port'));
 });
